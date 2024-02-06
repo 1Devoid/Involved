303144458ee7503a9d5b5034288a8a8668834abe
KachurovskyiD <kachurovskyi.d@gmail.com>
2024-02-06 10:22:01 +0200

    popup added

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import dropdownInit from './modules/dropdown.js';
 import popupInit from './modules/popup.js';
+import inputInit from './modules/input.js';
 
 dropdownInit();
 popupInit();
+inputInit();
---
+++ src/js/modules/input.js
@@ -0,0 +1,7 @@
+import { Input, initTE } from 'tw-elements';
+
+function inputInit() {
+  initTE({ Input });
+}
+
+export default inputInit;

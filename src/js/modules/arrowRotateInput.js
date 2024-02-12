23433fbc7f0d4faa48e0e852aae3a99963337546
KachurovskyiD <kachurovskyi.d@gmail.com>
2024-02-12 16:02:04 +0200

    edits

--- src/js/modules/arrowRotateInput.js
+++ src/js/modules/arrowRotateInput.js
@@ -2,7 +2,7 @@ function arrowRotateInput() {
   const ddAnimationBtn = document.querySelectorAll('.dd-animation');
   if (ddAnimationBtn) {
     ddAnimationBtn.forEach((btn) => {
-      btn.addEventListener('click', () => {
+      btn.addEventListener('click', (e) => {
         btn.classList.toggle('dd-animation-rotate');
       });
     });
@@ -10,3 +10,11 @@ function arrowRotateInput() {
 }
 
 export default arrowRotateInput;
+
+// if (
+//   e.target !== headerBurgerMenu &&
+//   e.target !== headerBurger &&
+//   e.target !== labelBurger
+// ) {
+//   headerBurger.checked = false;
+// }

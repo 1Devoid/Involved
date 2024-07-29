ed9514a8bd5cc814688bc899f8a52927bdccf504
KachurovskyiD <kachurovskyi.d@gmail.com>
2024-07-29 13:32:34 +0300

    edits

--- src/js/modules/setupActivityHandlers.js
+++ src/js/modules/setupActivityHandlers.js
@@ -1,34 +1,8 @@
 function setupActivityHandlers() {
-  if (document.getElementById('add-new-activity')) {
-    document
-      .getElementById('add-new-activity')
-      .addEventListener('click', function () {
-        document
-          .getElementById('new-activity-popup')
-          .classList.remove('hidden');
-      });
-
-    document
-      .getElementById('new-activity-cancel')
-      .addEventListener('click', function () {
-        document.getElementById('new-activity-popup').classList.add('hidden');
-        document
-          .getElementById('new-activity-error-msg')
-          .classList.remove('hidden');
-      });
-
-    document
-      .getElementById('new-activity-error-msg')
-      .addEventListener('click', function () {
-        document
-          .getElementById('new-activity-error-msg')
-          .classList.add('hidden');
-      });
-
+  if (document.getElementById('new-activity-submit')) {
     document
       .getElementById('new-activity-submit')
       .addEventListener('click', function () {
-        document.getElementById('new-activity-popup').classList.add('hidden');
         document.getElementById('skeleton').classList.remove('hidden');
 
         setTimeout(function () {
--- src/js/modules/setupActivityHandlers2.js
+++ src/js/modules/setupActivityHandlers2.js
@@ -1,30 +1,5 @@
 function setupActivityHandlers2() {
-  if (document.getElementById('add-new-activity-2')) {
-    document
-      .getElementById('add-new-activity-2')
-      .addEventListener('click', function () {
-        document
-          .getElementById('new-activity-popup')
-          .classList.remove('hidden');
-      });
-
-    document
-      .getElementById('new-activity-cancel-2')
-      .addEventListener('click', function () {
-        document.getElementById('new-activity-popup').classList.add('hidden');
-        document
-          .getElementById('new-activity-error-msg')
-          .classList.remove('hidden');
-      });
-
-    document
-      .getElementById('new-activity-error-msg')
-      .addEventListener('click', function () {
-        document
-          .getElementById('new-activity-error-msg')
-          .classList.add('hidden');
-      });
-
+  if (document.getElementById('new-activity-submit-2')) {
     document
       .getElementById('new-activity-submit-2')
       .addEventListener('click', function () {
@@ -40,7 +15,7 @@ function setupActivityHandlers2() {
           document
             .getElementById('new-activity-form')
             .classList.remove('hidden');
-          document.getElementById('new-activity-popup').classList.add('hidden');
+
           document
             .getElementById('new-activity-success-msg')
             .classList.remove('hidden');

4d4e0e43c49bca5f983243894eb47fe722e4d2a9
KachurovskyiD <kachurovskyi.d@gmail.com>
2024-11-01 18:03:17 +0200

    edits

--- src/js/modules/darkTheme.js
+++ src/js/modules/darkTheme.js
@@ -2,8 +2,18 @@ function darkThemeToggler() {
   const darkTogglerBtn = document.getElementById('darkToggler');
   const body = document.querySelector('.body');
 
+  if (sessionStorage.getItem('darkTheme') === 'enabled') {
+    body.classList.add('dark');
+  }
+
   darkTogglerBtn.addEventListener('click', () => {
     body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+      sessionStorage.setItem('darkTheme', 'enabled');
+    } else {
+      sessionStorage.setItem('darkTheme', 'disabled');
+    }
   });
 }
 
